ae41a2283b783a6ac070f1bc4c8756e268e2cc7f
theproblemcompany <office@officengom.studio>
2024-05-11 21:18:41 +0300

    Updates

---
+++ 48849f7bec5db4755f6f.js
@@ -0,0 +1,1 @@
+console.log("Loadhtml.js: Function running");const targetEl=document.querySelector(".target"),loadSnippet=t=>{fetch(`html${t}.html`).then((t=>{if(t.ok)return t.text()})).then((t=>{targetEl.innerHTML=t}))};window.onload=function(){setTimeout(console.log("Is this running?"),2e3),console.log("Loadhtml.js: Buttons loaded")};
