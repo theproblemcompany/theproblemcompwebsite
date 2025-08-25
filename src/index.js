e067f10c7811755da56af8c107fc436d62339583
theproblemcompany <hello@theproblemcompany.co>
2025-08-26 00:05:56 +0300

    updated

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './css/main.scss';
-import './css/blogpost_style.scss'
-import './css/services.scss'
-import './js/loadHeaderFooter.js';
-import './js/loadFilterBar.js'
-import csvPath from './quiz_response.csv';
